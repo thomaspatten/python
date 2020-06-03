88fb2f749b5e9b8ac76aa79173c56e84dadbbda6
thomaspatten <65768775+thomaspatten@users.noreply.github.com>
2020-06-03 15:33:47 +0100

    updated v0.2

--- code/grade_calc.py
+++ code/grade_calc.py
@@ -5,11 +5,11 @@ def grade_calc(physics, maths, english):
     
     if percentage<50:
         grade=("d")
-    elif percentage>50 and percentage<60:
+    elif percentage>=50 and percentage<60:
         grade= ("c")
-    elif percentage>60 and percentage<70:
+    elif percentage>=60 and percentage<70:
         grade= ("b")
-    elif percentage>70 and percentage<80:
+    elif percentage>=70 and percentage<80:
         grade= ("a")
 
     return grade
--- test/test_grade_calc.py
+++ test/test_grade_calc.py
@@ -1,7 +1,16 @@
 from grade_calc import grade_calc
 import pytest
 
-def test_one():
-    assert grade_calc(70, 70, 70) == (70)
+def test_a():
+    assert grade_calc(70, 70, 70) == ('a')
+    assert grade_calc(75,72,82)==('a')
 
-    
+def test_b():
+    assert grade_calc(60, 60, 60) == ('b')
+    assert grade_calc(69,70,70) ==('b')
+def test_c():
+    assert grade_calc(50,50,50)==('c')
+    assert grade_calc(59,60,60)==('c')
+def test_d():
+    assert grade_calc(40,40,40)==('d')
+    assert grade_calc(22,1,5)==('d')
